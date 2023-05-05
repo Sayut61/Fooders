6ef9ed57a2cbc19212400e87757f0dc689d1d9ba
Anton Sayutin <72465013+Sayut61@users.noreply.github.com>
2023-05-05 19:59:28 +0300

    0.0.9: добавил зависимости firebase, написал базовую реализацию скачивания изображений в MainFragment.kt

--- app/src/main/java/com/example/fooders/ui/mainScreen/MainFragment.kt
+++ app/src/main/java/com/example/fooders/ui/mainScreen/MainFragment.kt
@@ -1,16 +1,25 @@
 package com.example.fooders.ui.mainScreen
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fooders.databinding.FrMainBinding
+import com.google.firebase.storage.FirebaseStorage
 
 class MainFragment: Fragment() {
 
     private var _binding: FrMainBinding? = null
+
     private val binding get() = _binding!!
+
+    private val storage = FirebaseStorage.getInstance("gs://fooders-4d468.appspot.com")
+    private val storageRef = storage.reference
+    private val imageRef = storageRef.child("/main_menu_categories/ic_salad.jpg")
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +27,21 @@ class MainFragment: Fragment() {
     ): View? {
         _binding = FrMainBinding.inflate(inflater, container, false)
         setHasOptionsMenu(true)
+        val ONE_MEGABYTE = (1024 * 1024).toLong()
+        imageRef.getBytes(ONE_MEGABYTE)
+            .addOnSuccessListener { bytes ->
+                // Обработка успешной загрузки изображения
+                try {
+                    val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                    //binding.imageButton.setImageBitmap(bitmap)
+                } catch (e: Exception) {
+                    // Обработка ошибки
+                }
+            }
+            .addOnFailureListener {
+                // Обработка ошибки загрузки изображения
+            }
         return binding.root
+
     }
 }
